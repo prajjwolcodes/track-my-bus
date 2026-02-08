c55e895adf0a560ae672c0bd77acee998fa4b342
prajjwolcodes <shresthaprajjwol4@gmail.com>
2026-02-08 20:56:10 +0545

    firebase admin

--- firebase/firebase-messaging.ts
+++ firebase/firebase-messaging.ts
@@ -3,8 +3,6 @@
 import { getMessaging, getToken } from "firebase/messaging";
 import { firebaseApp } from "./firebase";
 
-
-
 export const generateToken = async () => {
     if (typeof window === "undefined") return;
 
@@ -16,23 +14,18 @@ export const generateToken = async () => {
             return;
         }
         const messaging = getMessaging(firebaseApp);
-
-
         let registration = await navigator.serviceWorker.getRegistration("/firebase-messaging-sw.js");
-
         if (!registration) {
             // console.log("Registering new service worker...");
             registration = await navigator.serviceWorker.register(
                 "/firebase-messaging-sw.js"
             );
-            // Wait for service worker to be ready
             await navigator.serviceWorker.ready;
         } else {
             // console.log("Using existing service worker registration");
         }
 
         const vapidKey = process.env.NEXT_PUBLIC_vapidKey;
-
         if (!vapidKey) {
             throw new Error("VAPID key is not defined. Check your .env file.");
         }
