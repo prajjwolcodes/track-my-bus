0cffa6fefdc71b58402f147cbc5018ff027f6c0f
Pratibha Sharma <pratibhaa625@gmail.com>
2026-05-09 22:30:59 +0545

    login register

--- app/landingpage/components/Hero.tsx
+++ app/landingpage/components/Hero.tsx
@@ -19,21 +19,24 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="bg-[#fcfaf2] px-2 py-20 md:py-20 lg:py-24 overflow-hidden">
+      className="bg-[#fcfaf2] px-2 py-16 md:py-20 lg:py-20 overflow-hidden">
       <div className="relative mx-auto w-full max-w-7xl sm:max-w-6xl rounded-4xl shadow-2xl overflow-visible">
 
-        {/* IMAGE */}
-        <div className="relative h-140 md:h-160 lg:h-160 w-full border-2 border-[#fdfbf2] rounded-3xl overflow-hidden">
+        <section className="relative h-[calc(100vh-64px)] w-full overflow-hidden">
           <Image
-            src="/bus.png"
-            alt="School Bus"
+            src="/logreg.png"
+            alt="Hero Background"
             fill
+            className="object-cover rounded-4xl"
             priority
-            className="object-cover object-left"
           />
-          <div className="absolute inset-0 bg-black/10" />
-        </div>
+        </section>
 
+    </div>
+    </section>
+  )
+}
+/* 
         <div
           className=" relative md:absolute md:bottom-8 md:right-0
     z-10 w-full md:w-[55%] lg:w-[50%] -mt-7.5 md:mt-0 bg-[#fdfbf2]/96
@@ -61,46 +64,43 @@ export default function Hero() {
             </span>
           </div>
 
-          {/* HEADING */}
-          <h1
-            className={`${libreBaskerville.className} text-2xl sm:text-3xl lg:text-4xl leading-tight text-[#1a1a1a]`}
-          >
-            Professional tracking for{" "}
-            <span className="italic text-blue-800">concerned parents</span> &
-            logistics operators
-          </h1>
+          // {/* HEADING */
+          // <h1
+          //   className={`${libreBaskerville.className} text-2xl sm:text-3xl lg:text-4xl leading-tight text-[#1a1a1a]`}
+          // >
+          //   Professional tracking for{" "}
+          //   <span className="italic text-blue-800">concerned parents</span> &
+          //   logistics operators
+          // </h1>
 
-          {/* DESCRIPTION */}
-          <p
-            className={`${nunito.className} mt-6 max-w-xl text-sm md:text-base leading-7 text-gray-600`}
-          >
-            SmartYatra provides real-time visibility, intelligent route
-            optimization, and instant safety alerts ensuring every student's
-            journey remains secure, transparent, and stress-free for schools,
-            parents, and drivers.
-          </p>
+          // {/* DESCRIPTION */}
+          // <p
+          //   className={`${nunito.className} mt-6 max-w-xl text-sm md:text-base leading-7 text-gray-600`}
+          // >
+          //   SmartYatra provides real-time visibility, intelligent route
+          //   optimization, and instant safety alerts ensuring every student's
+          //   journey remains secure, transparent, and stress-free for schools,
+          //   parents, and drivers.
+          // </p>
 
-          {/* BUTTONS */}
-          <div className="mt-10 flex flex-col gap-4 sm:flex-row">
-            <Link href="/signup">
-              <button className="group flex items-center justify-center gap-2 rounded-xl bg-blue-800 px-8 py-3.5 font-semibold text-white shadow-lg transition hover:bg-blue-900 hover:scale-[1.02]">
-                Get Started
-              </button>
-            </Link>
+          // {/* BUTTONS */}
+          // <div className="mt-10 flex flex-col gap-4 sm:flex-row">
+          //   <Link href="/signup">
+          //     <button className="group flex items-center justify-center gap-2 rounded-xl bg-blue-800 px-8 py-3.5 font-semibold text-white shadow-lg transition hover:bg-blue-900 hover:scale-[1.02]">
+          //       Get Started
+          //     </button>
+          //   </Link>
 
-            <a href="#how-it-works">
-              <button className="group flex items-center justify-center gap-2 rounded-xl border border-blue-200 px-8 py-3.5 font-semibold text-blue-900 transition hover:bg-blue-50 hover:scale-[1.02]">
-                Learn More
-                <ArrowRight
-                  className="transition-transform group-hover:translate-x-1"
-                  size={18}
-                />
-              </button>
-            </a>
-          </div>
-        </div>
+          //   <a href="#how-it-works">
+          //     <button className="group flex items-center justify-center gap-2 rounded-xl border border-blue-200 px-8 py-3.5 font-semibold text-blue-900 transition hover:bg-blue-50 hover:scale-[1.02]">
+          //       Learn More
+          //       <ArrowRight
+          //         className="transition-transform group-hover:translate-x-1"
+          //         size={18}
+          //       />
+          //     </button>
+          //   </a>
+          // </div>
+      //   </div>
 
-      </div>
-    </section>
-  );
-}
+      // </div>
